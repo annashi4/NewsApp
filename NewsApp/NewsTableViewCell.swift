fd3b7b3162174a6b483538c9fd033be490e4fb64
annashi4 <ann.shirinkina@gmail.com>
2022-07-12 10:25:29 +0400

    creating view model for table view

---
+++ NewsApp/NewsTableViewCell.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+class NewsTableViewCellViewModel {
+    let title: String
+    let subtitle: String
+    let imageURL: URL?
+    var imageData: Data? = nil
+    
+    init(title: String, subtitle: String, imageURL: URL?){
+        self.title = title
+        self.subtitle = subtitle
+        self.imageURL = imageURL
+    }
+}
+
+class NewsTableViewCell: UITableViewCell {
+
+    static let identifier = "NewsTableViewCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+    
+    func configure(with viewModel: NewsTableViewCellViewModel) {
+        
+    }
+    
+}
